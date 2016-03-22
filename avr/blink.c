c694f3fb9f65103ac03d0fc0519ee76ec58ec3a0
Bas Westerbaan <bas@westerbaan.name>
2016-03-22 22:49:04 +0100

    Adapt blink just to blink LED and left-over pin
    With blink we can measure the frequency of the uC

--- avr/blink.c
+++ avr/blink.c
@@ -1,39 +1,20 @@
-// Blink the LED on the programmer board
-// and send test messages over UART.
+// Blink the LED and free PIN on the programmer board.
 
-#define F_CPU 1154842
+#define F_CPU 1098206
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-static const long int BAUD_WAIT = 1000000 / 9600;
-
-static inline void write_byte(unsigned char what, unsigned char pin)
-{
-    PORTB &= ~(1 << pin); // start bit
-    _delay_us(BAUD_WAIT);
-    for (unsigned char i = 0; i < 8; i++) {
-        if (what & 1)
-            PORTB |= 1 << pin;
-        else
-            PORTB &= ~(1 << pin);
-        what >>= 1;
-        _delay_us(BAUD_WAIT);
-    }
-    PORTB |= 1 << pin;  // stop bit
-    _delay_us(BAUD_WAIT);
-}
 
 int main(void)
 {
     DDRB |= (1 << DDB4);  // LED-pin is output (pin 3)
-    DDRB |= (1 << DDB3);  // TX-pin is output (pin 2)
+    DDRB |= (1 << DDB3);  // Free pin is output (pin 2)
     PORTB |= (1 << DDB3); // start high
 
     while (1) {
-        // PINB |= (1 << DDB4);
-        write_byte('!', DDB3);
-        _delay_ms(1000);
+        PINB |= (1 << DDB4) | (1 << DDB3);
+        _delay_us(100);
     }
 
     return 0;
