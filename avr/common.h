a3517713f09de63655ca14bb9a95b87c43407595
Bas Westerbaan <bas@westerbaan.name>
2016-04-16 12:10:17 +0200

    common: increase DRAAD_DELAY *a lot*

--- avr/common.h
+++ avr/common.h
@@ -10,11 +10,11 @@
 #include <avr/io.h>
 
 // microsecond delay of the basic time period in the "draad" one-wire protocol.
-#define DRAAD_DELAY 30
+#define DRAAD_DELAY 200
 
 // microseconds it takes to pull the draad down.  Depends on the resistance
-// of the pull down resistor on the "draad".
-#define DRAAD_PULLDOWN_TIME 13
+// of the pull down resistor on the "draad".  Current is for a 100Kohm.
+#define DRAAD_PULLDOWN_TIME 4
 
 // Set the ATTINY13 clock to ~9.4MHz, by clearing the clock divisor
 inline void full_speed_clock()
