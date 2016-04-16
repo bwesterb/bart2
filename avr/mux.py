7501bc844557488dfa5192927a220f1598965deb
Bas Westerbaan <bas@westerbaan.name>
2016-04-16 12:09:57 +0200

    mux: decode SPI frames

--- avr/mux.py
+++ avr/mux.py
@@ -13,12 +13,33 @@ def main():
     spi.bits_per_word = 8
     spi.max_speed_hz = 10000
 
+    def _bin(x):
+        return bin(x)[2:].zfill(8)
+
+
     def send(who, what):
         b1 = (who << 6) | (8 << 1) | 1
         b2 = what
-        print map(bin, spi.xfer(map(rev, [b1,b2])))
+        print map(_bin, spi.xfer(map(rev, [b1,b2])))
 
-    send(0, 0b01101001)
+    send(0, 0b1000000000000000)
+    time.sleep(0.050)
+    s = ''.join(map(_bin, spi.xfer([0]*80)))
+    i = 0
+    print s
+    while i < len(s):
+        if s[i] == '0':
+            i += 1
+            continue
+        i += 1
+        print ''.join(reversed(s[i:i+5]))
+        l = int(''.join(reversed(s[i:i+5])), 2)
+        i += 5
+        w = s[i:i+2]
+        i += 2
+        d = s[i:i+l]
+        i += l
+        print w,':', l, d
 
 if __name__ == '__main__':
     main()
